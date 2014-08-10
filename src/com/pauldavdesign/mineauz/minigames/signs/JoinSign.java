9ecbc9f11968ea36e8096722e681994182c0efa0
itstake <bexco2010@gmail.com>
2014-08-10 18:58:26 +0900

    Join 표지판 4번째줄 추가, 표지판 베이스 몇가지 수정

--- src/com/pauldavdesign/mineauz/minigames/signs/JoinSign.java
+++ src/com/pauldavdesign/mineauz/minigames/signs/JoinSign.java
@@ -43,6 +43,19 @@ public class JoinSign implements MinigameSign {
 		if(plugin.mdata.hasMinigame(event.getLine(2))){
 			event.setLine(1, ChatColor.GREEN + "들어가기");
 			event.setLine(2, plugin.mdata.getMinigame(event.getLine(2)).getName());
+			if(Minigames.plugin.hasEconomy()){
+				if(!event.getLine(3).isEmpty() && !event.getLine(3).matches("\\$?[0-9]+(.[0-9]{2})?")){
+					event.getPlayer().sendMessage(ChatColor.RED + "알수없는 돈 액수!");
+					return false;
+				}
+				else if(event.getLine(3).matches("[0-9]+(.[0-9]{2})?")){
+					event.setLine(3, event.getLine(3) + "원");
+				}
+			}
+			else{
+				event.setLine(3, "");
+				event.getPlayer().sendMessage(ChatColor.RED + "돈을 사용하려면 Vayle 가 필요합니다!");
+			}
 			return true;
 		}
 		event.getPlayer().sendMessage(ChatColor.RED + "\"" + event.getLine(2) + "\" 라는 이름을 가진 미니게임이 없습니다!");
@@ -55,6 +68,16 @@ public class JoinSign implements MinigameSign {
 			Minigame mgm = plugin.mdata.getMinigame(sign.getLine(2));
 			if(mgm != null && (!mgm.getUsePermissions() || player.getPlayer().hasPermission("minigame.join." + mgm.getName().toLowerCase()))){
 				if(mgm.isEnabled()){
+					if(!sign.getLine(3).isEmpty() && Minigames.plugin.hasEconomy()){
+						double amount = Double.parseDouble(sign.getLine(3).replace("원", ""));
+						if(Minigames.plugin.getEconomy().getBalance(player.getName()) >= amount){
+							Minigames.plugin.getEconomy().withdrawPlayer(player.getName(), amount);
+						}
+						else{
+							player.sendMessage(ChatColor.RED + "[PMGO-L] " + ChatColor.WHITE + "당신은 이 미니게임에 들어갈 만한 돈이 없습니다!");
+							return false;
+						}
+					}
 					plugin.pdata.joinMinigame(player, mgm);
 					return true;
 				}
